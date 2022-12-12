e34c020c08d0923714dc54f91842dc9e009c2af5
adlund97 <112456558+adlund97@users.noreply.github.com>
2022-12-11 22:46:58 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -136,7 +136,7 @@ async function getUserPosts(uID) {
 async function getUser (uID) {
 	if (!uID) return;
 	try {
-		const user = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${uID}`);
+		const user = await fetch(`https://jsonplaceholder.typicode.com/users/${uID}`);
 		if (!user) throw new Error("Couldn't retrieve user id data.");
 		
 		
