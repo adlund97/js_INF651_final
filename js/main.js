c068fafff869330df957f865d19ea8ad0c92555b
adlund97 <112456558+adlund97@users.noreply.github.com>
2022-12-11 23:19:19 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -176,30 +176,28 @@ async function displayComments (postID) {
 async function createPosts (postData) {
 	if (!postData) return;
 	const fragment = document.createDocumentFragment();
-	const article = document.createElement('article');
 	postData.forEach(async (post) => {
-		
+		const article = document.createElement('article');
 		const header2 = createElemWithText('h2', post.title);
 		const para1 = createElemWithText('p', post.body);
 		const para2 = createElemWithText('p', `Post ID: ${post.id}`);
-		const author = await getUser(post.userId);
-		const para3 = createElemWithText('p', `Author: ${author.name} with ${author.company.name}`);
-		const para4 = createElemWithText('p', `${author.company.catchPhrase}`);
+		//const author = await getUser(post.userId);
+		//const para3 = createElemWithText('p', `Author: ${author.name} with ${author.company.name}`);
+		//const para4 = createElemWithText('p', `${author.company.catchPhrase}`);
 		const myButton = createElemWithText('button', 'Show Comments');
 		myButton.dataset.postId = post.id;
 		
 		article.append(header2);
 		article.append(para1);
 		article.append(para2);
-		article.append(para3);
-		article.append(para4);
+		//article.append(para3);
+		//article.append(para4);
 		article.append(myButton);
 		
 		const section = await displayComments(post.id);
 		article.append(section);
-		
+		fragment.append(article);
 	});
-	fragment.append(article);
 	return fragment;
 }
 
